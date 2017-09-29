9f47e2004be0136b69c8d9a24a110f779c40d46d
StinaTjejerKodar <stina_ae@yahoo.se>
2017-09-29 11:17:13 +0200

    #5 KOD till databas i settings
    Ändrat i databas

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        )
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
