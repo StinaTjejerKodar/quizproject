675e68a31098c3aac93ea22fbd1dd647fd5517b7
StinaTjejerKodar <stina_ae@yahoo.se>
2017-09-29 10:14:20 +0200

    #1 #2 #3
    #1 Dags att skapa en tom sajt hos heroku
    
    #2 Förbered sajten för Heroku genom att skapa en MASSA filer
    
    #3 Se till att våra filer i static-mappen fungerar som de ska på Heroku

--- quiz/views.py
+++ quiz/views.py
@@ -10,7 +10,7 @@ def startsida(request):
 
 def quiz(request, quiz_number):
 	context = {
-	    "quiz": Quiz.objects.get(quiz_number=quiz_number),
+		"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
 
@@ -21,34 +21,48 @@ def question(request, quiz_number, question_number):
 	questions = quiz.questions.all()
 	question = questions[question_number - 1]
 	context = {
-    		"question_number": question_number,
-    		"question": question.question,
-	    	"answer1": question.answer1,
-    		"answer2": question.answer2,
-	    	"answer3": question.answer3,
-	    	"quiz": quiz,
+			"question_number": question_number,
+			"question": question.question,
+			"answer1": question.answer1,
+			"answer2": question.answer2,
+			"answer3": question.answer3,
+			"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 
 	return render(request, "question.html", context)
 
 def results(request, quiz_number):
-	context = {
-	    	"correct": 12,
-	    	"total": 20,
-		"quiz_number": quiz_number,
-	}
-	return render(request, "results.html", context)
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = list(quiz.questions.all())
+	saved_answers = request.session.get(str(quiz_number), {})
+	
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+		if correct_answer == answer:
+			num_correct_answers = num_correct_answers + 1
 
-def results(request, quiz_number):
-		return render(request, "results.html")
+	num_questions = quiz.questions.count()
+	context = {
+		"correct": num_correct_answers,
+		"total": num_questions,
+	}
+
+	return render(request, "results.html", context)
 
 def answer(request, quiz_number, question_number):
 	answer = request.POST["answer"]
 	saved_answers = request.session.get(str(quiz_number), {})
 	saved_answers[question_number] = int(answer)
 	request.session[quiz_number] = saved_answers
-	return redirect("question_page", quiz_number, question_number + 1)
+	
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("results_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
 
 
 
